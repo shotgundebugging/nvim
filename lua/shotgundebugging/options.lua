2178f5c419c1d412c72b5aaa915cb3983b8ec0c7
Mircea <3748026+shotgundebugging@users.noreply.github.com>
2026-02-27 12:16:05 +0200

    Allow jump by words separated with _

--- lua/shotgundebugging/options.lua
+++ lua/shotgundebugging/options.lua
@@ -3,3 +3,6 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
+
+-- Treat snake_case parts as separate words for motions/text objects.
+vim.opt.iskeyword:remove '_'
